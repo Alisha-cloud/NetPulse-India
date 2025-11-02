e21a9458aba47bfc081d04ee358c0511a0027591
Alisha Khan <alisha810495@gmail.com>
2025-11-02 18:46:36 +0530

    Connected with Render backend API

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //  Base API instance
 const API = axios.create({
-  baseURL: "http://localhost:8080/api", 
+  baseURL: "https://netpulse-indiaba.onrender.com", 
 });
 
 //Events
